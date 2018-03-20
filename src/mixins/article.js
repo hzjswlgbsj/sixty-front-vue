d48f0deeb51a53f5fd5875fae05ae67ee9877c21
liulin <liulin@corp-ci.com>
2018-03-20 19:15:43 +0800

    将数据存储方式改成，先初始化然后读取缓存

--- src/mixins/article.js
+++ src/mixins/article.js
@@ -1,45 +1,19 @@
 import dataStore from '../data/index'
 import arrayTool from '../util/array'
-import articleApi from '../api/article'
-import tagsApi from '../api/tags'
 
-async function getArticles () {
-  try {
-    let articles = await articleApi.all()
-    if (articles && articles.length > 0) {
-      dataStore.store('articles', articles)
-      let allArticles = dataStore.store('articles')
-      for (let article of allArticles) {
-        let tagIds = article.tag_ids && article.tag_ids.split(',')
-        article.tags = await getTagsByIds(tagIds)
-      }
-      console.log(11111111111)
-      console.log(allArticles)
-      console.log(111111111111)
-      return allArticles
-    }
-  } catch (e) {
-    console.log(e)
-  }
+function getArticles () {
+  return dataStore.store('articles')
 }
 
-async function getTags () {
-  try {
-    let tags = await tagsApi.all()
-    if (tags && tags.length > 0) {
-      dataStore.store('article_tags', tags)
-      return dataStore.store('article_tags')
-    }
-  } catch (e) {
-    console.log(e)
-  }
+function getTags () {
+  return dataStore.store('article_tags')
 }
 
-async function getTagsByIds (ids) {
+function getTagsByIds (ids) {
   if (!ids || !arrayTool.isArray(ids) || ids.length < 1) {
     return null
   }
-  const allTags = await getTags()
+  const allTags = getTags()
   let tags = []
   for (let tagId of ids) {
     for (let tag of allTags) {
--- src/page/Blog.vue
+++ src/page/Blog.vue
@@ -17,6 +17,8 @@ import FootBar from './layout/FootBar'
 import ArticleItem from '../components/ArticleItem'
 import routerMixin from '../mixins/router'
 import dataStore from '../../src/data/index'
+import articleApi from '../api/article'
+import tagsApi from '../api/tags'
 import articleMixin from '../mixins/article'
 
 export default {
@@ -49,7 +51,35 @@ export default {
       dataStore.store('curRouter', this.$route)
       let curRouterObj = dataStore.store('curRouter')
       this.isDetail = curRouterObj.name === 'ArticleDetail'
-      this.articleData = await articleMixin.getArticles() // get all article data
+      this.getArticle() // Initialize article data and set store
+      this.getArticleTagsById() // Initialize articleTags data and set store
+    },
+    async getArticle () {
+      try {
+        let articles = await articleApi.all()
+        if (articles && articles.length > 0) {
+          dataStore.store('articles', articles) // 初始化store里面的文章数据
+          let allArticles = dataStore.store('articles') // 此处可以深拷贝出来再修改，然后再存到store，可以用lodash，本项目小博客我就不引用那么多东西了
+          for (let article of allArticles) {
+            let tagIds = article.tag_ids && article.tag_ids.split(',')
+            article.tags = articleMixin.getTagsByIds(tagIds)
+          }
+          this.articleData = articles
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    async getArticleTagsById () {
+      try {
+        let tags = await tagsApi.all()
+        if (tags && tags.length > 0) {
+          dataStore.store('article_tags', tags) // 初始化store里面的标签数据
+          this.tags = dataStore.store('article_tags')
+        }
+      } catch (e) {
+        console.log(e)
+      }
     },
     goDetail (id) {
       this.jump(`blog/articleDetail/${id}`)
