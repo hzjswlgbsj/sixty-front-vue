54e63876ced0046cf731a5b41d9f858db1d464d1
Liulin <hiliulin@aliyun.com>
2018-04-27 15:04:31 +0800

    接微博第三方测试, v1.0.5

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,5 +8,5 @@ module.exports = merge(prodEnv, {
   WEIBO_DOMAIN: '"https://api.weibo.com"',
   WEIBO_APPKEY: '"1979573568"',
   WEIBO_APPSECRET: '"6adce1e216516ad5f961be9a3cac70bc"',
-  WEIBO_REDIRECT: '"http://www.sixtyden.com"'
+  WEIBO_REDIRECT: '"http://www.sixtyden.com/login"'
 })
