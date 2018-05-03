86abab368302d0064b9b45d764c10725231f72b6
Liulin <liulin@corp-ci.com>
2018-05-03 09:35:14 +0800

    测试第三方登录

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -41,11 +41,17 @@ export default {
             /* 通过access_token和uid获取微博用户的详细信息 */
             let userInfo = await weiboTokenApi.getUser(tokenInfo.access_token, parseInt(tokenInfo.uid))
             console.log('userInfo', userInfo)
-            if (userInfo && userInfo.id === String(tokenInfo.uid)) {
+            if (userInfo && userInfo.id === parseInt(tokenInfo.uid)) {
               /* 将获取到的微博用户信息注册到本应用的用户系统中去 */
-              let registerResult = await usersApi.register(userInfo.screen_name, userInfo.profile_image_url, 1, userInfo.idstr)
-              if (registerResult) {
-                this.$Message.success('授权成功')
+              let userResult = await usersApi.all(userInfo.id)
+              if (userResult && userResult.id) {
+                dataStore.setCookie('userInformation', userResult)
+              } else {
+                /* 将获取到的微博用户信息注册到本应用的用户系统中去 */
+                let registerResult = await usersApi.register(userInfo.screen_name, userInfo.profile_image_url, 1, userInfo.id)
+                if (registerResult) {
+                  this.$Message.success('授权成功')
+                }
               }
             }
           }
@@ -56,6 +62,9 @@ export default {
         /* 通过用户授权得到下一步获取access_token接口所需要的code */
         window.location.href = `https://api.weibo.com/oauth2/authorize?client_id=${env.WEIBO_APPKEY}&response_type=code&redirect_uri=${env.WEIBO_REDIRECT}`
       }
+    },
+    async getUser () {
+
     }
   }
 }
