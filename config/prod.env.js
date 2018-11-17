dad05f43329ac71e5a33c219f0d9db1f2b45850c
Linlin <hiliulin@aliyun.com>
2018-11-17 16:48:54 +0800

    屏蔽重要配置文件

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_DOMAIN: '"http://api.sixtyden.com"',
+  API_DOMAIN: '""', // 请换成你的
   WEIBO_DOMAIN: '"https://api.weibo.com"',
-  WEIBO_APPKEY: '"1979573568"',
-  WEIBO_APPSECRET: '"6adce1e216516ad5f961be9a3cac70bc"',
-  WEIBO_REDIRECT: '"http://www.sixtyden.com/login"',
+  WEIBO_APPKEY: '""', // 请换成你的
+  WEIBO_APPSECRET: '""', // 请换成你的
+  WEIBO_REDIRECT: '""', // 请换成你的
   WEIBO_GET_TOKEN_URL: '"https://api.weibo.com/oauth2/access_token"',
   WEIBO_GET_USER_URL: '"https://api.weibo.com/2/users/show.json"'
 }
