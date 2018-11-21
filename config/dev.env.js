1bb8a2b62338f24842b416855f6bde0541d90c55
Liu lin <hiliulin@aliyun.com>
2018-11-21 09:58:11 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,9 +6,9 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_DOMAIN: '"http://api.sixty.com"',
   WEIBO_DOMAIN: '"https://api.weibo.com"',
-  WEIBO_APPKEY: '"1979573568"',
-  WEIBO_APPSECRET: '"6adce1e216516ad5f961be9a3cac70bc"',
-  WEIBO_REDIRECT: '"http://www.sixtyden.com/login"',
+  WEIBO_APPKEY: '""', // 改成你的
+  WEIBO_APPSECRET: '""', // 改成你的
+  WEIBO_REDIRECT: '""', // 改成你的
   WEIBO_GET_TOKEN_URL: '"https://api.weibo.com/oauth2/access_token"',
   WEIBO_GET_USER_URL: '"https://api.weibo.com/2/users/show.json"'
 })
