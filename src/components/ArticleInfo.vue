19e5f97b6a7fee63d9d5c0f203446bfad48263b0
Linlin <hiliulin@aliyun.com>
2018-12-22 23:29:42 +0800

    为文章信息添加显示评论数

--- src/components/ArticleInfo.vue
+++ src/components/ArticleInfo.vue
@@ -4,10 +4,17 @@
       <icon name="at" scale="0.8"></icon>
       <span class="article-info-author icon">{{article.nickname}}&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;</span>
     </div>
+
+    <div class="article-info">
+      <icon name="comments" scale="0.8"></icon>
+      <span class="article-info-comment icon">23 comments&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;</span>
+    </div>
+
     <div class="article-info">
       <icon name="terminal" scale="0.8"></icon>
       <span class="article-info-date icon"><timeago :datetime="article.create_time" :auto-update="60"/>&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;</span>
     </div>
+
     <div class="article-info article-info-address">
       <icon name="location-arrow" scale="0.8"></icon>
       <span class="article-info-address icon">南京&nbsp;•&nbsp;谷阳世纪大厦</span>
